ce0dce165d001a14bd07699aacf635f5f467ed65
smartsystemav <hello@smartsystemav.co.uk>
2026-06-13 10:33:17 +0100

    Update banner image path in home.tsx

--- artifacts/midnight-culture/src/pages/home.tsx
+++ artifacts/midnight-culture/src/pages/home.tsx
@@ -17,7 +17,7 @@ import {
 } from "lucide-react";
 
 import logoImg from "../assets/images/New MC Logo 26 JD.png";
-import bannerImg from "../assets/images/New MC Logo 26 JD.png";
+import bannerImg from "artifacts/midnight-culture/src/assets/images/New MC JD Logo No BG.PNG";
 import bgImg from "@assets/MC_Background_1776245648247.png";
 import bandImg from "@assets/DSC03883_1776245820156.jpg";
 
